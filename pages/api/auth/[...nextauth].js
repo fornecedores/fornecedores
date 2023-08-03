8aa655187f0421dc9fe91c87566a75ffe267ca75
gestao_eventus <systemsmanagerlda@gmail.com>
2023-08-03 08:31:48 +0100

    Alteracoes nas imagens

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,34 +1,34 @@
-import clientPromise from '@/lib/mongodb';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import NextAuth, { getServerSession } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import clientPromise from "@/lib/mongodb";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth, { getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const adminEmails = ['smartmanagerlda@gmail.com'];
+const adminEmails = ["coutinhocoutinholucas@gmail.com"];
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    session: ({session,token,user}) => {
+    session: ({ session, token, user }) => {
       if (adminEmails.includes(session?.user?.email)) {
         return session;
       } else {
         return false;
       }
-    }
-  }
-}
+    },
+  },
+};
 export default NextAuth(authOptions);
-export async function isAdminRequest(req,res) {
-  const session = await getServerSession(req,res,authOptions);
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
   if (!adminEmails.includes(session?.user?.email)) {
     res.status(401);
     res.end();
-    throw 'not admin'
+    throw "not admin";
   }
 }
